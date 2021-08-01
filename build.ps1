8c20f7ca491e4c048acd6c3207b9d335723a29b8
Stefan Hoffmann <stho32github@gmail.com>
2021-08-01 10:41:57 +0200

    Build now creates a subdirectory output which contains the pdf

--- build.ps1
+++ build.ps1
@@ -5,7 +5,13 @@ $dateiliste = Get-ChildItem -Path $directory *.md -Recurse | ForEach-Object {
     $_.Fullname.Replace($directory + "/", "")
 } | Where-Object { $_ -ne "index.md" } | Sort-Object
 
-pandoc ($dateiliste) -o ../abteilungswe-v3.pdf `
+if (Test-Path "../output") {
+    Remove-Item "../output" -Recurse -Force
+}
+
+New-Item "../output" -ItemType Directory
+
+pandoc ($dateiliste) -o ../output/abteilungswe-v3.pdf `
     --toc `
     -V toc-title:"Inhaltsverzeichnis" `
     -V linkcolor:blue `
