da143fa99455c58d129d909747523882d021a563
stho32 <stho32github@gmail.com>
2021-12-07 19:46:07 +0100

    Update in overleaf

--- create-filelist-for-github-action-fr.ps1
+++
@@ -1,9 +0,0 @@
-$directory = $PSScriptRoot 
-Set-Location $directory
-
-$dateiliste = Get-ChildItem -Path $directory *_fr.md -Recurse | ForEach-Object {
-    $_.Fullname.Replace($directory + "/", "")
-} | Where-Object { $_ -ne "README.md" } | Sort-Object
-
-$dateiliste -join " "
-
